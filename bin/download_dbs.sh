471bdbe61716e22a0c74ff769fb64379f3045e93
Bjorn Wallner <bjornwallner@gmail.com>
2017-02-24 13:27:48 +0100

    fixed install dir

--- bin/download_dbs.sh
+++ bin/download_dbs.sh
@@ -2,4 +2,6 @@
 #wget http://bioinfo.ifm.liu.se/ProQ2/cache.tar.gz
 wget http://bioinfo.ifm.liu.se/ProQ2/DB.tar.gz
 wget http://bioinfo.ifm.liu.se/ProQ2/sspro4.db.tar.gz
+tar -xvzf sspro4.db.tar.gz
+tar -xvzf DB.tar.gz
 
